491b187796600774e71a208f01ed8ebad2184fc9
timba <timjackmaster@gmail.com>
2022-11-18 09:00:47 +0100

    Defining interface following .net system sorting interface

---
+++ TJMSorts/ISorter.cs
@@ -0,0 +1,17 @@
+﻿namespace TJMSorts;
+
+//Interface specifying the methods that a sorter class must implement
+public interface ISorter
+{
+    //uses default comparer
+    public List<T> Sort<T>(List<T> list) where T : IComparable<T>;
+        
+    //uses custom comparer
+    public List<T> Sort<T>(List<T> list, IComparer<T> comparer);
+        
+    //uses specified comparison
+    public List<T> Sort<T>(List<T> list, Comparison<T> comparison);
+    
+    //sorts elements in a range of the list
+    public List<T> Sort<T>(List<T> list, IComparer<T> comparer, int startIndex, int count);
+}
