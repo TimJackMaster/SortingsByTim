3bf7ca259bc548330607b94e6c9062b514116221
timba <timjackmaster@gmail.com>
2022-11-18 12:54:18 +0100

    Added Exception Handling to prevent IndexOutOfRange exceptions

--- TJMSorts/Sorter.cs
+++ TJMSorts/Sorter.cs
@@ -31,6 +31,15 @@ internal sealed class Sorter : ISorter
 
     public List<T> Sort<T>(List<T> list, IComparer<T> comparer, int startIndex, int count)
     {
+        if (startIndex < 0)
+            throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex must be greater than or equal to zero.");
+        if (count < 0)
+            throw new ArgumentOutOfRangeException(nameof(count), "count must be greater than or equal to zero.");
+        if (startIndex + count > list.Count)
+            throw new ArgumentException("startIndex + count must be less than or equal to the length of the list.");
+        if (startIndex > list.Count)
+            throw new ArgumentException("startIndex must be less than or equal to the length of the list.");
+
         return _algorithm.Sort(new List<T>(list), comparer, startIndex, count);
     }
 }
