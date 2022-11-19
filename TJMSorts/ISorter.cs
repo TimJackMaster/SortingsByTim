c8c8df4b3cbebfec0f27edf78b67d2c866dae34e
timba <timjackmaster@gmail.com>
2022-11-19 11:58:20 +0100

    Fixed minor mistake in bubble sort; refactored order of parameters in method to fit order of c# default sort

--- TJMSorts/ISorter.cs
+++ TJMSorts/ISorter.cs
@@ -30,15 +30,15 @@ public interface ISorter
     /// <typeparam name="T">Type of elements in the list</typeparam>
     /// <returns>A sorted list of type T</returns>
     public List<T> Sort<T>(List<T> list, Comparison<T> comparison);
-    
+
     /// <summary>
     /// Sorts the range of elements in the List using the specified comparer.
     /// </summary>
     /// <param name="list">The original list</param>
-    /// <param name="comparer">In implementation of the comparer for T</param>
     /// <param name="startIndex">The starting index of the range</param>
     /// <param name="count">The amount of items to be sorted</param>
+    /// <param name="comparer">In implementation of the comparer for T</param>
     /// <typeparam name="T">Type of elements in the list</typeparam>
     /// <returns>A sorted list of type T</returns>
-    public List<T> Sort<T>(List<T> list, IComparer<T> comparer, int startIndex, int count);
+    public List<T> Sort<T>(List<T> list, int startIndex, int count, IComparer<T> comparer);
 }
--- TJMSorts/Sorter.cs
+++ TJMSorts/Sorter.cs
@@ -29,17 +29,17 @@ internal sealed class Sorter : ISorter
         return _algorithm.Sort(new List<T>(list), comparison);
     }
 
-    public List<T> Sort<T>(List<T> list, IComparer<T> comparer, int startIndex, int count)
+    public List<T> Sort<T>(List<T> list, int startIndex, int count, IComparer<T> comparer)
     {
         if (startIndex < 0)
             throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex must be greater than or equal to zero.");
+        if (startIndex > list.Count)
+            throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex must be less than or equal to the length of the list.");
         if (count < 0)
             throw new ArgumentOutOfRangeException(nameof(count), "count must be greater than or equal to zero.");
         if (startIndex + count > list.Count)
             throw new ArgumentException("startIndex + count must be less than or equal to the length of the list.");
-        if (startIndex > list.Count)
-            throw new ArgumentException("startIndex must be less than or equal to the length of the list.");
 
-        return _algorithm.Sort(new List<T>(list), comparer, startIndex, count);
+        return _algorithm.Sort(new List<T>(list), startIndex, count, comparer);
     }
 }
