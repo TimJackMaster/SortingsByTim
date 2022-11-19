69adc7cf920919b0c9eaa62fdbc77e931b4e7e34
timba <timjackmaster@gmail.com>
2022-11-19 13:06:58 +0100

    Adding Service Extension for DI registration and a factory for direct object creation

---
+++ TJMSorts/ServiceExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TJMSorts.Algorithms;
+using TJMSorts.Algorithms.Components;
+
+namespace TJMSorts;
+
+public static class ServiceExtensions
+{
+    private static SortingOptions _options = new();
+    
+    public static IServiceCollection RegisterSorting(this IServiceCollection services, SortingOptions options)
+    {
+        _options = options;
+
+        services = services.RegisterComponents();
+        services = services.RegisterAlgorithm();
+        services.AddTransient<ISorter, Sorter>();
+        
+        return services;
+    }
+
+    private static IServiceCollection RegisterComponents(this IServiceCollection services)
+    {
+        services.AddTransient<IComparisonEncapsulation, ComparisonEncapsulation>();
+        services.AddTransient<ISwapEncapsulation, SwapEncapsulation>();
+
+        return services;
+    }
+
+    private static IServiceCollection RegisterAlgorithm(this IServiceCollection services)
+    {
+        switch (_options.Algorithm)
+        {
+            case Algorithm.BubbleSort:
+                services.AddTransient<ISortingAlgorithm, BubbleSort>();
+                break;
+            default:
+                throw new Exception("Algorithm not implemented");
+        }
+
+        return services;
+    }
+}
---
+++ TJMSorts/SortingOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace TJMSorts;
+
+public class SortingOptions
+{
+    public const string Sorting = "Sorting";
+    
+    public Algorithm Algorithm { get; set; }
+}
