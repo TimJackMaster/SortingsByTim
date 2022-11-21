4daaf5a769fedc1a7a9e7ee3ae21bb2d7fb49683
timba <timjackmaster@gmail.com>
2022-11-21 16:02:29 +0100

    Argument Objects to use as "Event" Args

---
+++ TJMSorts/EventArguments/UpdatingListArgs.cs
@@ -0,0 +1,22 @@
+﻿namespace TJMSorts.EventArguments;
+
+/// <summary>
+/// Arguments when more than two elements changed position.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public sealed class UpdatingListArgs<T> : EventArgs
+{
+    /// <summary>
+    /// Copy of the list after the change.
+    /// </summary>
+    public List<T> NewList { get; }
+
+    /// <summary>
+    /// ctor
+    /// </summary>
+    /// <param name="list">List after the change.</param>
+    public UpdatingListArgs(List<T> list)
+    {
+        NewList = new List<T>(list);
+    }
+}
