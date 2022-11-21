77f2947fc36a8ee84bff34e79dbf2b855ccf09bb
timba <timjackmaster@gmail.com>
2022-11-21 16:02:29 +0100

    Argument Objects to use as "Event" Args

---
+++ TJMSorts/EventArguments/ComparingArgs.cs
@@ -0,0 +1,27 @@
+﻿namespace TJMSorts.EventArguments;
+
+/// <summary>
+/// Arguments when two elements are compared.
+/// </summary>
+public sealed class ComparingArgs : EventArgs
+{
+    /// <summary>
+    /// Index of the first element.
+    /// </summary>
+    public int Index1 { get; }
+    /// <summary>
+    /// Index of the second element.
+    /// </summary>
+    public int Index2 { get; }
+
+    /// <summary>
+    /// ctor
+    /// </summary>
+    /// <param name="index1">Index of the first element</param>
+    /// <param name="index2">Index of the second element</param>
+    public ComparingArgs(int index1, int index2)
+    {
+        Index1 = index1;
+        Index2 = index2;
+    }
+}
